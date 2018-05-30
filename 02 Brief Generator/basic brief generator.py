221527096e3f1a1da69bdbb99cc73f5e879d730c
David Benqué <mail@davidbenque.com>
2018-05-30 22:44:54 +0100

    add mermaid graph to basic brief

--- 02 Brief Generator/basic brief generator.py
+++ 02 Brief Generator/basic brief generator.py
@@ -2,7 +2,7 @@
 # @Author: davidbenque
 # @Date:   2018-03-29 11:14:25
 # @Last Modified by:   davidbenque
-# @Last Modified time: 2018-05-30 16:19:35
+# @Last Modified time: 2018-05-30 22:40:21
 
 from py2neo import Graph
 import itertools
@@ -44,18 +44,18 @@ start = entity.current()
 start_id = start["a"]["node_id"]
 entity_node = graph.find_one("Entity", "node_id", start_id)
 
-import sys
-sys.stdout = open('file.md', 'w')
+
+file = open('file.md', 'w')
 
 # Company Details
-print("#" + start["a"]["name"])
+file.write("#" + start["a"]["name"] + "\n")
 
 basic_info = ["company_type", "status", "address"]
 
 for deet in basic_info:
     if entity_node[deet]:
-        print(deet.title() + ": " + entity_node[deet]) 
-print("")
+        file.write(deet.title() + ": " + entity_node[deet] + "\n") 
+file.write(""+ "\n")
 
 
 # Generator check 
@@ -72,67 +72,117 @@ incoming_nodes = graph.match(end_node = entity_node)
 in_check = peek(incoming_nodes)
 
 if in_check is None:
-    print("")
+    file.write("" + "\n")
 else:
-    print("--- Incoming ---")
+    file.write("##Incoming" + "\n")
     first, nodelist = in_check
     for i in nodelist:
         link_type = i.type()
         if link_type.endswith("_OF"):
             link_type = link_type[:-3]
-        print(link_type)
+        file.write(link_type + "\n")
         source_dict = parse_node(i.start_node())
         for key, i in source_dict.items():
             if i:
-                print(i)
-        print("")
+                file.write(i + "\n" )
+        file.write("" + "\n" + "\n")
 
 # Outgoing links 
 outgoing_nodes = graph.match(start_node = entity_node)
 out_check = peek(outgoing_nodes)
 
 if out_check is None:
-    print("")
+    file.write("" + "\n")
 else:
-    print("--- Outgoing ---")
+    file.write("##Outgoing" + "\n")
     first, nodelist = out_check
     for i in outgoing_nodes:
         link_type = i.type()
         if link_type.endswith("_OF"):
             link_type = link_type[:-3]
-        print(link_type)
+        file.write(link_type)
 
         source_dict = parse_node(i.end_node())
         for key, i in source_dict.items():
             if i:
-                print(i)
-        print("")
+                file.write(i + "\n")
+        file.write("" + "\n" + "\n") 
 
 
 # Network
-print("--- Graph ---")
+file.write("##Graph" + "\n")
 
-print("```mermaid")
-print("graph TD")
+file.write("```mermaid" + "\n")
+file.write("graph LR" + "\n")
 
-target = parse_node(entity_node)
-target_graphnode = target["name"]
+file.write("classDef outline fill:#fff,stroke:#000,stroke-width:1px;" + "\n")
 
-print("target[" + target_graphnode + "]")
-print("class target outline")
-print("classDef outline fill:#fff,stroke:#000,stroke-width:1px;")
+node_index = 1
+graph_nodes = {}
+node_queue = []
 
-source_index = 1
 
-for i in graph.match(end_node = entity_node):
-    source = parse_node(i.start_node())
-    source_graphnode = source["name"] + "<br>" + source["countries"]
-    source_ref = "source" + str(source_index)
-    source_index += 1
-    print(source_ref + "[" + source_graphnode + "]")
-    print(source_ref + "-->|" + i.type() + "|target")
+def graph_node_label(node):
+    try:
+        a = node["name"]
+    except:
+        a = node["countries"]
 
-    #print(source["name"], "-->|" + i.type())
+    try:
+        b = node["countries"]
+    except:
+        b = ""
 
-print("```")
+    return a + "<br>" + str(b)
+
+
+def add_graph_node(graph_node, node_dict):
+    global node_index, graph_nodes
+    if graph_node not in graph_nodes.values():
+        graph_nodes["node" + str(node_index)] = graph_node.replace("(", "\(")
+        node_index += 1
+
+        node_name = list(graph_nodes.keys())[list(graph_nodes.values()).index(graph_node)]
+
+        file.write(node_name + "[" + graph_node + "]" + "\n")
+
+    node_name = list(graph_nodes.keys())[list(graph_nodes.values()).index(graph_node)]
+    return node_name
+
+
+def graphnode(input_node):
+    global node_index, graph_nodes
+
+    node = parse_node(input_node)
+    graphnode = graph_node_label(node)
+    node_name = add_graph_node(graphnode, graph_nodes)
+    
+
+    for i in graph.match(end_node = input_node):
+        node_queue.append(i.start_node())
+
+        source = parse_node(i.start_node())
+        source_graphnode = graph_node_label(source)
+
+        source_name = add_graph_node(source_graphnode, graph_nodes)
+        file.write(source_name + "-->|" + i.type() + "|" +  node_name + "\n")
+
+    for i in graph.match(start_node = input_node):
+
+        target = parse_node(i.end_node())
+        target_graphnode = graph_node_label(target)
+
+        target_name = add_graph_node(target_graphnode, graph_nodes)
+        file.write(node_name + "-->|" + i.type() + "|" +  target_name + "\n")
+
+
+
+graphnode(entity_node)
+file.write("class node1 outline" + "\n")
+
+for node in node_queue:
+    graphnode(node)
+
+
+file.write("```")
 
