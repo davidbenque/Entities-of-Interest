3eb35ddb0916cb8b7b3034dc20010a3f97eae040
David Benqué <mail@davidbenque.com>
2018-06-13 12:09:12 +0100

    draw basic bokeh graph example

--- 02-Brief Generator/basic-bokeh-graph.py
+++ 02-Brief Generator/basic-bokeh-graph.py
@@ -1,20 +1,28 @@
 
 import networkx as nx
 
+# %%
+
 G = nx.Graph()
 
-nodes = [(1,'one'), (2,'two'), (3, 'three'), (4, 'four')]
-links = [(1,2), (1,3), (1,4), (2,4)]
+nodes = [(0,{'name':'boso'}), (1,{'name':'bipo'}), (2, {'name':'bopu'}), (3, {'name':'one'})]
+links = [(0,2), (0,3), (1,0), (2,0)]
 
 for n in nodes:
-    G.add_node(n[0], attr_dict=n[1])
+    G.add_node(n[0], name = n[1]["name"])
 
 for edge in links:
     G.add_edge(edge[0], edge[1])
 
+# %%
+
+G.nodes(data=True)
+
+# %%
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models.graphs import from_networkx
+from bokeh.models.graphs import from_networkx, NodesAndLinkedEdges, EdgesAndLinkedNodes
+from bokeh.models import HoverTool, Oval, ColumnDataSource, LabelSet
 
 plot = figure(title="Networkx Integration Demonstration",
             plot_width=800,
@@ -23,9 +31,22 @@ plot = figure(title="Networkx Integration Demonstration",
             toolbar_location="right",
             tools="pan,wheel_zoom,box_zoom,reset")
 
-bokeh_graph = from_networkx(G, nx.spring_layout, scale=80)
+hover = HoverTool(tooltips=[("Name", "@name")])
+plot.add_tools(hover)
 
+bokeh_graph = from_networkx(G, nx.spring_layout, scale=80)
+bokeh_graph.node_renderer.glyph = Oval(height=4, width=4)
+bokeh_graph.inspection_policy = NodesAndLinkedEdges()
 plot.renderers.append(bokeh_graph)
+
+x, y = zip(*bokeh_graph.layout_provider.graph_layout.values())
+node_labels = nx.get_node_attributes(G, 'name')
+source = ColumnDataSource({'x': x, 'y': y,
+                           'name': [node_labels[i] for i in range(len(x))]})
+labels = LabelSet(x='x', y='y', text='name', source=source,
+                  background_fill_color='white')
+
+plot.renderers.append(labels)
 
 output_file("networkx_graph.html")
 show(plot)
